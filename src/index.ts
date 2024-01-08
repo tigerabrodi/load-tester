722b9ef4fa556480981b738a2bd09b9031a4a31d
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-08 07:41:24 +0100

    feat: options for url and number of requests

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,18 @@ import { Command } from 'commander'
 
 const program = new Command()
 
-program.argument('<url>', 'URL to make HTTP request to').action((url) => {
-  http.get(url, (response) => {
-    console.log(`Response code: ${response.statusCode}`)
-  })
-})
+program
+  .option('-u, --url <type>', 'URL to make HTTP requests to')
+  .option('-n, --number <type>', 'Number of requests to make', '1')
 
 program.parse(process.argv)
+
+const options = program.opts()
+const numberOfRequests = parseInt(options.number, 10)
+
+for (let i = 0; i < numberOfRequests; i++) {
+  // Make HTTP request to options.url
+  http.get(options.url, (response) => {
+    console.log(`Response code: ${response.statusCode}`)
+  })
+}
