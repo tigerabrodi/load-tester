7de4c656508238fa6daa969310c488cd6b059df0
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-08 07:37:29 +0100

    request to a URL

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+import http from 'http'
+
+import { Command } from 'commander'
+
+const program = new Command()
+
+program.argument('<url>', 'URL to make HTTP request to').action((url) => {
+  http.get(url, (response) => {
+    console.log(`Response code: ${response.statusCode}`)
+  })
+})
+
+program.parse(process.argv)
