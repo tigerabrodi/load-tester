fcf6bfa48c5784c939d75d31acdb4e4483d21679
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-08 14:00:06 +0100

    more delay of test server

--- src/testServer.ts
+++ src/testServer.ts
@@ -3,8 +3,13 @@ import http from 'http'
 const SERVER_PORT = 8080
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Response from Server 1\n')
+  // Generate a random delay between 500ms and 2000ms
+  const delay = Math.random() * (2000 - 500) + 500
+
+  setTimeout(() => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Response from Server 1\n')
+  }, delay)
 })
 
 server.listen(SERVER_PORT, () => {
