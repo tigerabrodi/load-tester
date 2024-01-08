df1a33b1eb01b71839169702aa117e7796cff3fc
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-08 14:00:32 +0100

    refactor displayFormattedStats: already formatted time

--- src/displayFormattedStats.ts
+++ src/displayFormattedStats.ts
@@ -1,37 +1,21 @@
 import type { CalculatedStats } from './calculateStats'
 
 export function displayFormattedStats(stats: CalculatedStats) {
-  const formatTime = (time: number) => time.toFixed(2)
-
   console.log('Results:')
   console.log(` Total Requests (2XX).......................: ${stats.total2XX}`)
   console.log(` Failed Requests (5XX)......................: ${stats.total5XX}`)
   console.log(
-    ` Request/second.............................: ${formatTime(
-      stats.requestsPerSecond
-    )}`
+    ` Request/second.............................: ${stats.requestsPerSecond}`
   )
 
   console.log('')
   console.log(
-    `Total Request Time (s) (Min, Max, Mean).....: ${formatTime(
-      stats.totalRequestTime.min
-    )}, ${formatTime(stats.totalRequestTime.max)}, ${formatTime(
-      stats.totalRequestTime.mean
-    )}`
+    `Total Request Time (s) (Min, Max, Mean).....: ${stats.totalRequestTime.min}, ${stats.totalRequestTime.max}, ${stats.totalRequestTime.mean}`
   )
   console.log(
-    `Time to First Byte (s) (Min, Max, Mean).....: ${formatTime(
-      stats.timeToFirstByte.min
-    )}, ${formatTime(stats.timeToFirstByte.max)}, ${formatTime(
-      stats.timeToFirstByte.mean
-    )}`
+    `Time to First Byte (s) (Min, Max, Mean).....: ${stats.timeToFirstByte.min}, ${stats.timeToFirstByte.max}, ${stats.timeToFirstByte.mean}`
   )
   console.log(
-    `Time to Last Byte (s) (Min, Max, Mean)......: ${formatTime(
-      stats.timeToLastByte.min
-    )}, ${formatTime(stats.timeToLastByte.max)}, ${formatTime(
-      stats.timeToLastByte.mean
-    )}`
+    `Time to Last Byte (s) (Min, Max, Mean)......: ${stats.timeToLastByte.min}, ${stats.timeToLastByte.max}, ${stats.timeToLastByte.mean}`
   )
 }
